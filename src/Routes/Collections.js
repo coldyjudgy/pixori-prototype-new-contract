e39ad2a5858fe4f019b5f5e4255ba0a2a97e7781
kwklly <yeonlee@i.ewha.ac.kr>
2021-07-19 10:15:47 +0900

    disable Collection2 without login

--- src/Routes/Collections.js
+++ src/Routes/Collections.js
@@ -1,14 +1,22 @@
 import {TokenCluster2} from '../clusters/token-cluster2'
 import {useCurrentUser} from '../hooks/current-user'
 import React from 'react'
+import {ErrorTokenCluster} from '../clusters/error-token-cluster'
 
 export default () => {
 
 function Token() {
     const cu = useCurrentUser()
+    if(cu.addr) {
     return (
       <TokenCluster2 addresss={"0xdb16a5e14c410280"} address={cu.addr} />
     )
+    } 
+    else {
+      return (
+        <ErrorTokenCluster />
+      )
+    }
   }
 
   return (
